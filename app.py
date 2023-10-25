837d2921b9b08b4efbf0487b88ca5e6f998d8ac0
AstridZhao <zhao26z@mtholyoke.edu>
2023-10-25 18:08:39 -0400

    new code able to run

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import streamlit as st
 import os
 import subprocess
 from typing import List
+from transformers import AutoTokenizer
+import requests
+import json
 
 # Local embedding model for embedding the question.
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -24,12 +27,6 @@ vector_store = Epsilla(
     db_name="LocalChatDB"
 )
 vector_store.use_collection("LocalChatCollection")
-
-# Initialize Llama2
-from transformers import AutoTokenizer
-import requests
-import json
-
 model_name = 'TheBloke/Llama-2-7b-Chat-GGUF/resolve/main/llama-2-7b-chat.Q4_K_M.gguf'
 pure_name = model_name.split('/')[-1]
 parts = model_name.split('/')
@@ -39,7 +36,7 @@ model_path = f"{parts[0]}/{parts[1]}"
 # --------------------------------
 # install llama.cpp if don't have
 # install model if you don't have
-# --------------------------------
+# --------------------------------'
 
 st.title("💬 Moho Bot")
 if "messages" not in st.session_state:
@@ -50,41 +47,72 @@ for msg in st.session_state.messages:
 if question := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": question})
     
-    # WaitReview: add vector search: 
+    
     # https://github.com/epsilla-cloud/app-gallery/blob/main/local-chatbot/app.py
     #https://medium.com/@anoopjohny2000/building-a-conversational-chat-interface-with-streamlit-and-langchain-for-csvs-8c150b1f982d?source=read_next_recirc-----a5053b16f85----2---------------------81756fa8_2a08_42cb_bb9e_43cd23b2ae3a-------
 
-    #TODO: FIX vector_store.similarity_search
+    #TODO:  vector_store.similarity_search
+    # https://python.langchain.com/docs/modules/data_connection/vectorstores/
     context = '\n'.join(map(lambda doc: doc.page_content, vector_store.similarity_search(question, k = 5)))
+    # context = '\n'.join(map(lambda doc: doc.page_content, vector_store.similarity_search(question, k = 5)))
+    # context = vector_store
+    print(context)
     st.chat_message("user").write(question)
     
     prompt = f'''
-        You are helpful assistant to answer the user question based on the given information. 
-        You can get some help from the given context to help you to summerize your answer.
-        Please don't make things up or say things not mentioned in the Context. 
-        Ask for more information when needed. 
+        You are a helpful question answer assistant. 
         
-        Question: 
-            {question}
-        Context: 
-            {context}
-        '''
-
+        Answer the question followed the rules:
+        1. Do not copy the context in your answer.
+        2. Try to understand the Context and rephrase them.
+        3. Please don't make things up or say things not mentioned in the Context. 
+        4. If you don't know the answer, just say you don't know, and ask the user if they are interested in to know something else.
+        5. The context you got is the most updated information, you should trust it.
+        
+        
+        Based on given Context: "{context}", answer question: {question}
+    '''
+    
+    # print(prompt)
     # WaitReview: call model
     os.chdir('/Users/astridz/Documents/llama.cpp')
-    # command = ./main -m llama-2-7b-chat.Q4_K_M.gguf  -n 1024 -ngl 48
-    # command = ['./main', '-m', pure_name, '-ngl 48', prompt]
-    # ./main -m ./models/7B/ggml-model-q4_0.bin -n 1024 --repeat_penalty 1.0 --color -i -r "User:" -f ./prompts/chat-with-bob.txt
-    command = f"./main -m {pure_name} -c 1024 -ngl 48 {prompt}"
-    process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+   # command = f"./main -m {pure_name} -c 1024 -ngl 48 -p '{prompt}'"
+    # ./main -m llama-2-7b-chat.Q4_K_M.gguf -c 1024 -ngl 48 -p
+    command = ['./main', '-m', pure_name, '-c', '2048', '-ngl', '48', '-p', prompt]
+    # print(base_command)
+    # print(command)
     
-    output = process.stdout.read()
+    #TODO: subprocess 
+    # process = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     content = ''
-    if output:
-        content = content + output
-    # Change back to the parent directory using Python
-    msg = { 'role': 'assistant', 'content': content }
+    
+    #Testing: which can print corrent output
+    # try:
+    #     outs, errs = process.communicate(timeout=15)
+    #     print("communicated")
+    # except subprocess.TimeoutExpired:
+    #     process.kill()
+    #     outs, errs = process.communicate()
+    
+    while True:
+        output = process.stdout.readline()
+        if output:
+            content = output
+        return_code = process.poll()
+        if return_code is not None:
+            break
+    
+    # result = []
+    # result.append(content.pop())
+    # result.append(content.pop())
+    # results = " ".join(result)
+    
+    
+    print("answer is : \n", content)
+    msg = { 'role': 'assistant', 'content':content}
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg['content'])
+    
     os.chdir('/Users/astridz/Documents/Moho_Bot')
     
--- learn.py
+++ learn.py
@@ -37,7 +37,17 @@ loader = WebBaseLoader(web)
 cs_data = loader.load()
 cs_data_s = cs_data[0].page_content
 
-# Run in terminal :
+#Testing:
+print(os.getcwd())
+if (os.getcwd() != "/Users/astridz/Documents/Moho_Bot/Documents_collection"):
+    os.chdir('Documents_collection')
+writing(cs_data_s, "cs_page.txt")
+os.chdir('../')
+
+# # Run in terminal :
+# if (os.getcwd() != "/Users/astridz"):
+#     os.chdir('/Users/astridz')
+
 # docker pull epsilla/vectordb
 # docker run --pull=always -d -p 8888:8888 epsilla/vectordb
 
@@ -51,64 +61,64 @@ else:
 nltk.download('punkt')
 nltk.download('stopwords')
 
-def preprocessing(data, stopping, stemming):# Tokenize each document in the list
-    tokenized_documents = []
-    trans = str.maketrans('','', string.punctuation)
-    sentences = nltk.sent_tokenize(data)  # Tokenize the document into sentences
-    for each in sentences:
-            #Tokenization: remove punctuation
-            each = each.translate(trans)
-            #content clear
-            each = each.replace ("\n", " ")
-            #Case folding: make lowercase
-            each = each.lower()
-            words = [nltk.word_tokenize(each)]
-            # Tokenize each sentence into words
-            tokenized_documents.extend(words)
+# def preprocessing(data, stopping, stemming):# Tokenize each document in the list
+#     tokenized_documents = []
+#     trans = str.maketrans('','', string.punctuation)
+#     sentences = nltk.sent_tokenize(data)  # Tokenize the document into sentences
+#     for each in sentences:
+#             #Tokenization: remove punctuation
+#             each = each.translate(trans)
+#             #content clear
+#             each = each.replace ("\n", " ")
+#             #Case folding: make lowercase
+#             each = each.lower()
+#             words = [nltk.word_tokenize(each)]
+#             # Tokenize each sentence into words
+#             tokenized_documents.extend(words)
 
-    stemmer = PorterStemmer()
-    stoplist = set(stopwords.words('english'))
-    eachline = []
-    output = []
+#     stemmer = PorterStemmer()
+#     stoplist = set(stopwords.words('english'))
+#     eachline = []
+#     output = []
     
-    for sentences in tokenized_documents:
-            #Stopping & Normalization option
-            if (stopping == True):
-                filtered_stop = [word for word in sentences if word not in stoplist]
-                if (stemming == True):
-                    stem_words = filtered_stop
-                    stem_words  = [stemmer.stem(w) for w in stem_words]
-                    eachline= ' '.join(stem_words)
-                else:
-                    eachline = ' '.join(filtered_stop)
-            else:
-                if (stemming == True):
-                    stem_words = sentences
-                    stem_words  = [stemmer.stem(w) for w in stem_words]
-                    eachline= ' '.join(stem_words)
-                else:
-                    eachline= each
-            eachline = [eachline]
-            output.extend(eachline)
-    # print(output)
-    output_string = " ".join(output)
-    return  output_string
+#     for sentences in tokenized_documents:
+#             #Stopping & Normalization option
+#             if (stopping == True):
+#                 filtered_stop = [word for word in sentences if word not in stoplist]
+#                 if (stemming == True):
+#                     stem_words = filtered_stop
+#                     stem_words  = [stemmer.stem(w) for w in stem_words]
+#                     eachline= ' '.join(stem_words)
+#                 else:
+#                     eachline = ' '.join(filtered_stop)
+#             else:
+#                 if (stemming == True):
+#                     stem_words = sentences
+#                     stem_words  = [stemmer.stem(w) for w in stem_words]
+#                     eachline= ' '.join(stem_words)
+#                 else:
+#                     eachline= each
+#             eachline = [eachline]
+#             output.extend(eachline)
+#     # print(output)
+#     output_string = " ".join(output)
+#     return  output_string
 
-# store tokenizing data into ".txt" file
-def create_db(data, filename):
-    if (os.getcwd() != "/Users/astridz/Documents/Moho_Bot/Documents"):
-        os.chdir('Documents')
+# # store tokenizing data into ".txt" file
+# def create_db(data, filename):
+#     if (os.getcwd() != "/Users/astridz/Documents/Moho_Bot/Documents_collection"):
+#         os.chdir('Documents_collection')
         
-    data_string = data[0].page_content
-    data_string = data_string.replace("\n", "")
-    clean_data = preprocessing(data_string, True, False)
+#     data_string = data[0].page_content
+#     data_string = data_string.replace("\n", "")
+#     clean_data = preprocessing(data_string, True, False)
     
-    writing(clean_data, filename)
-    os.chdir('../')
-    AssertionError(os.getcwd() != "/Users/astridz/Documents")
+#     writing(clean_data, filename)
+#     os.chdir('../')
+#     AssertionError(os.getcwd() != "/Users/astridz/Documents/Moho_Bot")
 
-filename = "cs_page.txt"
-create_db(cs_data, filename)
+# filename = "cs_page.txt"
+# create_db(cs_data, filename)
 
 # Local embedding model
 model = SentenceTransformer('all-MiniLM-L6-v2')
@@ -121,13 +131,16 @@ embeddings = LocalEmbeddings()
 
 # Get list of all files in "./documents/"
 # Testing: remove # !
-files = glob("./documents/*")
+files = glob("./Documents_collection/*")
 for file in files:
     loader = TextLoader(file)
     documents = loader.load()
-    splitted_documents = RecursiveCharacterTextSplitter(
-        chunk_size=100, 
-        chunk_overlap=20).split_documents(documents)
+    #TODO: use CharacterTextSplitter when use ORIGINAL data document
+    # Use Holistic view: Understanding the overall meaning of the text; 
+    # Identifying the relationships between different parts of the text; Generating new text
+    splitted_documents = CharacterTextSplitter(
+        chunk_size=1200, 
+        chunk_overlap=500).split_documents(documents)
 
     client = vectordb.Client()
 
